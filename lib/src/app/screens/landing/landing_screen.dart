736f85c7bef606a87e12193dd447b0270b00b058
Taeho Hwang <hth225@gmail.com>
2021-04-26 21:47:15 +0900

    Fix landing screen

--- lib/src/app/screens/landing/landing_screen.dart
+++ lib/src/app/screens/landing/landing_screen.dart
@@ -14,19 +14,58 @@ class _LandingScreenState extends ViewState<LandingScreen, LandingController> {
   // TODO: implement view
   Widget get view {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            child: Text(
-              'Did you make your bed?',
-              style: Theme.of(context).textTheme.headline6,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(bottom: 12),
+                                child: Text(
+                                  'Did you make your bed?',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline5
+                                      .copyWith(
+                                          color: Colors.black87,
+                                          fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Container(
+                                child: Text(
+                                  'Make your bed, Make your day.',
+                                  style: Theme.of(context).textTheme.subtitle2,
+                                ),
+                              ),
+                            ],
+                          )),
+                      Expanded(flex: 1, child: Container()),
+                    ]),
+              ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Get Start', style: Theme.of(context).textTheme.button),
-          )
-        ],
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.all(16),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Get Start',
+                    style: Theme.of(context).textTheme.button),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/src/app/theme/theme.dart
+++ lib/src/app/theme/theme.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 
 ThemeData buildLightThemeData() {
   return ThemeData(
-    primaryColor: Colors.lightBlue,
-    accentColor: Colors.lightBlueAccent
+    primaryColor: Colors.black,
+    accentColor: Colors.black,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(Colors.black87),
+      overlayColor: MaterialStateProperty.all<Color>(Colors.black54),
+    )),
+    textTheme: TextTheme(
+        button: TextStyle(
+            fontWeight: FontWeight.bold, color: Colors.white, fontSize: 16)),
   );
 }
